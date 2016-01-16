b2625f5e23b7710889a28f08afc1ba071d21a862
Marcin Morawski <marcin@morawskim.pl>
2016-01-16 11:50:26 +0100

    add ruby ext repository

---
+++ manifests/zypper/repositories/ruby_extensions.pp
@@ -0,0 +1,23 @@
+class mopensuse::zypper::repositories::ruby_extensions {
+  
+  include mopensuse::zypper::refresh
+  
+  $gpg_key = "http://download.opensuse.org/repositories/devel:/languages:/ruby:/extensions/openSUSE_${::operatingsystemrelease}/repodata/repomd.xml.key"
+  
+  zypprepo {'devel_languages_ruby_extensions':
+    baseurl      => "http://download.opensuse.org/repositories/devel:/languages:/ruby:/extensions/openSUSE_${::operatingsystemrelease}/",
+    enabled      => 1,
+    autorefresh  => 0,
+    descr        => "Ruby Extensions (openSUSE_${::operatingsystemrelease})",
+    gpgcheck     => 1,
+    gpgkey       => $gpg_key,
+    type         => 'rpm-md',
+    notify       => Class['mopensuse::zypper::refresh']
+  }
+  
+  rpmkey {'0E9AF123':
+    ensure  => present,
+    source  => $gpg_key,
+    require => Zypprepo['devel_languages_ruby_extensions']
+  }
+}
