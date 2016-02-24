4ee7da561756b41e01fc8aea0e60177c64573db5
Marcin Morawski <marcin@morawskim.pl>
2016-02-24 19:53:08 +0100

    vagrant - setup VAGRANT_HOME env and sudoers file for nfs share

--- manifests/packages/vagrant.pp
+++ manifests/packages/vagrant.pp
@@ -1,4 +1,7 @@
-class mopensuse::packages::vagrant {
+class mopensuse::packages::vagrant(
+  $vagrant_nfsd_users,
+  $vagrant_home = '/usr/share/vagrant'
+) {
   
   include mopensuse::zypper::repositories::morawskim
   
@@ -16,5 +19,33 @@ class mopensuse::packages::vagrant {
     ensure  => link,
     target  => "/opt/vagrant/embedded/gems/gems/vagrant-${vagrant_version}/contrib/bash/completion.sh",
     require => [ Package['vagrant'], Class['mopensuse::packages::bash'] ]
+  }
+  
+  group { 'vagrant':
+    ensure => 'present',
+  }
+
+  file { $vagrant_home:
+    ensure   => directory,
+    mode     => '2775',
+    owner    => 'root',
+    group    => 'vagrant',
+    require  => Group['vagrant']
+  }
+  
+  augeas { "/files/etc/environment/VAGRANT_HOME":
+    changes => [
+      "set /files/etc/environment/VAGRANT_HOME '$vagrant_home'",
+    ],
+    require => [ Package["augeas"], File[$vagrant_home] ]
+  }
+  
+  file { '/etc/sudoers.d/vagrant':
+    ensure  => present,
+    mode    => '0740',
+    owner   => 'root',
+    group   => 'root',
+    content => template("${module_name}/vagrant/sudoers.erb"),
+    require => Package['vagrant']
   }
 }
