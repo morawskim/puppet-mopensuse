eb3e9d389f08c6a56a378c90a960e30b812213fe
Marcin Morawski <marcin@morawskim.pl>
2016-03-23 19:52:41 +0100

    mysql - query log

--- manifests/packages/mysql.pp
+++ manifests/packages/mysql.pp
@@ -21,11 +21,23 @@ class mopensuse::packages::mysql(
     service_manage          => true, #must be true
     root_password           => $mysql_root_password,
     remove_default_accounts => true,
+    includedir              => '/etc/my.cnf.d',
+    purge_conf_dir          => false,
     require                 => Package['mariadb']
   }
   
   class { '::mysql::client':
     package_ensure => present,
     package_name   => 'mariadb-tools',
+  }
+
+  file {'/etc/my.cnf.d/log_query.cnf':
+    ensure  => present,
+    mode    => '0640',
+    owner   => 'root',
+    group   => 'mysql',
+    content => template("${module_name}/mysql/log_query.cnf.erb"),
+    notify  => Service['mysql'],
+    require => Package['mariadb']
   }
 }
