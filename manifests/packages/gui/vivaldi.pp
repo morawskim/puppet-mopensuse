06e72b68875c22f703a3220c83564efca98948cb
Marcin Morawski <marcin@morawskim.pl>
2016-04-09 09:36:10 +0200

    vivalid - repo_add_once

--- manifests/packages/gui/vivaldi.pp
+++ manifests/packages/gui/vivaldi.pp
@@ -1,9 +1,25 @@
 class mopensuse::packages::gui::vivaldi {
   
   include mopensuse::zypper::repositories::vivaldi
+  include mopensuse::packages::augeas
   
   package{'vivaldi-stable':
     ensure  => present,
     require => Class['mopensuse::zypper::repositories::vivaldi'],
+  }
+  
+  file { '/etc/default/vivaldi':
+    ensure  => 'present',
+    group   => 'root',
+    mode    => '644',
+    owner   => 'root',
+    require => Package['vivaldi-stable']
+  }
+  
+  augeas { "/files/etc/default/vivaldi/repo_add_once":
+    changes => [
+    'set /files/etc/default/vivaldi/repo_add_once \'"true"\'',
+    ],
+    require => [ Package["augeas"], File['/etc/default/vivaldi'] ]
   }
 }
