b6fca52d2b3098871eaf724b932b136058944a9a
Marcin Morawski <marcin@morawskim.pl>
2016-04-13 18:14:56 +0200

    create symlink to built-in php bin/config

--- manifests/packages/mphp.pp
+++ manifests/packages/mphp.pp
@@ -1,16 +1,39 @@
 class mopensuse::packages::mphp {
-  
+
   include mopensuse::zypper::repositories::morawskim
-  
+  include mopensuse::packages::php
+
   augeas { "/files/etc/environment/PHP_VERSIONS":
     changes => [
       'set /files/etc/environment/PHP_VERSIONS \'"/opt/php"\'',
     ],
     require => [ Package["augeas"] ]
   }
-    
+
   package {'mphp-switcher':
     ensure  => present,
-    require => Class['mopensuse::zypper::repositories::morawskim']
+    require => [
+        Class['mopensuse::zypper::repositories::morawskim'],
+        Class['mopensuse::packages::php']
+    ]
   }
+
+  file {'/opt/php/php/etc/php5':
+    ensure  => link,
+    target  => "/etc/php5",
+    require => Package['mphp-switcher']
+  }
+
+  file {'/opt/php/php/usr/bin/php':
+      ensure  => link,
+      target  => "/usr/bin/php",
+      require => Package['mphp-switcher']
+  }
+
+  file {'/opt/php/php/usr/bin/php-config':
+      ensure  => link,
+      target  => "/usr/bin/php-config",
+      require => Package['mphp-switcher']
+  }
+
 }
