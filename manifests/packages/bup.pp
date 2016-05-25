3dc709b3d227fdb7582ea924296199581fbf51e0
Marcin Morawski <marcin@morawskim.pl>
2016-05-25 18:54:40 +0200

    bup package require additional python repository

--- manifests/packages/bup.pp
+++ manifests/packages/bup.pp
@@ -1,9 +1,24 @@
-class mopensuse::packages::bup {
-  
+class mopensuse::packages::bup(
+    $bup_dir='/srv/bup'
+) {
+
   include mopensuse::zypper::repositories::morawskim
- 
+  include mopensuse::zypper::repositories::devel-languages-python
+
   package {'bup':
     ensure  => present,
-    require => Class['mopensuse::zypper::repositories::morawskim']
+    require => [
+        Class['mopensuse::zypper::repositories::morawskim'],
+        Class['mopensuse::zypper::repositories::devel-languages-python']
+    ]
   }
+
+  file { $bup_dir:
+    ensure   => directory,
+    mode     => '1755',
+    owner    => 'root',
+    group    => 'root',
+    require  => Package['bup']
+  }
+
 }
---
+++ manifests/zypper/repositories/devel-languages-python.pp
@@ -0,0 +1,24 @@
+class mopensuse::zypper::repositories::devel-languages-python {
+    
+    include mopensuse::zypper::refresh
+    
+    $gpg_key = "http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_${::operatingsystemrelease}/repodata/repomd.xml.key"
+    
+    zypprepo {'devel-languages-python':
+        baseurl      => "http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_${::operatingsystemrelease}/",
+        enabled      => 1,
+        autorefresh  => 1,
+        descr        => "Python Modules (openSUSE_${::operatingsystemrelease})",
+        gpgcheck     => 1,
+        gpgkey       => $gpg_key,
+        type         => 'rpm-md',
+        notify       => Class['mopensuse::zypper::refresh']
+    }
+    
+    rpmkey {'EDF0D733':
+        ensure  => present,
+        source  => $gpg_key,
+        before  => Zypprepo['devel-languages-python']
+    }
+    
+}
