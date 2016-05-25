4852e937af673fb414c9c060a7915bf74101540b
Marcin Morawski <marcin@morawskim.pl>
2016-05-25 18:40:39 +0200

    vivaldi import gpg key fix

--- manifests/zypper/repositories/vivaldi.pp
+++ manifests/zypper/repositories/vivaldi.pp
@@ -3,7 +3,7 @@ class mopensuse::zypper::repositories::vivaldi {
   include mopensuse::zypper::refresh
   
   $gpg_key = "https://gist.githubusercontent.com/morawskim/dcee4dd70958c4e9ede389c45f7f8d5f/raw/c47323f1cd395b5c79d0fc23cd2bdb19b6b302df/vivaldi_gpg.pub"
-  $gpg_future_key = 'https://gist.githubusercontent.com/morawskim/34a411a7e10c73ae79d89c00b2b644bb/raw/509f0a2e22e1bf42baf6dc4b3470bd7181f716b6/vivaldi_future_gpg.pub'
+  $gpg_future_key = 'https://gist.githubusercontent.com/morawskim/34a411a7e10c73ae79d89c00b2b644bb/raw/c198f2b4b130cb574665d3eae16cf1cde4d6e6b9/vivaldi_future_gpg.pub'
   
   zypprepo {'vivaldi':
     baseurl      => "http://repo.vivaldi.com/archive/rpm/x86_64",
