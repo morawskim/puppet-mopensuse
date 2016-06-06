3f59e3db1e247f42f56b69b8f03efd1a988bf8d3
Marcin Morawski <marcin@morawskim.pl>
2016-06-06 17:33:18 +0200

    samba smb.conf fix

--- manifests/packages/samba.pp
+++ manifests/packages/samba.pp
@@ -1,7 +1,8 @@
 class mopensuse::packages::samba {
   
   include mopensuse::packages::firewall
-  
+  include samba::server
+
   #managed by samba module
   #package {'samba':
   #  ensure => present
@@ -14,7 +15,12 @@ class mopensuse::packages::samba {
   #  enable      => true,
   #  require     => Package['samba']
   #}
-  
+
+  package {'samba-doc':
+    ensure  => present,
+    require => Package['samba']
+  }
+
   #firewall samba server
   exec {'firewall_open_samba_server_port':
     command => 'sysconf_addword /etc/sysconfig/SuSEfirewall2 FW_CONFIGURATIONS_EXT samba-server',
@@ -23,7 +29,7 @@ class mopensuse::packages::samba {
     require => [ Package['samba'], Class['mopensuse::packages::firewall'] ],
     notify  => Class['mopensuse::services::firewall']
   }
-  
+
   file {'/srv/samba':
     ensure  => directory,
     mode    => '1755',
@@ -31,7 +37,7 @@ class mopensuse::packages::samba {
     group   => 'root',
     before  => Package['samba']
   }
-  
+
   file {'/srv/samba/public':
     ensure  => directory,
     mode    => '1777',
@@ -39,9 +45,13 @@ class mopensuse::packages::samba {
     group   => 'root',
     require => File['/srv/samba']
   }
-  
-  
-  
-  include samba::server
-  
+
+  file {'/etc/samba/dhcp.conf':
+      ensure   => present,
+      mode     => '644',
+      owner    => 'root',
+      group    => 'root',
+      require  => Package['samba'],
+      notify   => Class['samba::server']
+  }
 } 
