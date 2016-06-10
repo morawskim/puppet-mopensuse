da76bbb7f256fb906317544edab6a33d9a67ca41
Marcin Morawski <marcin@morawskim.pl>
2016-06-10 17:52:41 +0200

    pygments

--- manifests/packages/colout.pp
+++ manifests/packages/colout.pp
@@ -1,6 +1,7 @@
 class mopensuse::packages::colout {
     
     include mopensuse::zypper::repositories::utilities
+    include mopensuse::packages::pygments
     
     package {'colout':
         ensure  => present,
---
+++ manifests/packages/pygments.pp
@@ -0,0 +1,10 @@
+class mopensuse::packages::pygments {
+    
+    include mopensuse::zypper::repositories::devel-languages-python
+    
+    package {'python-Pygments':
+        ensure  => present,
+        require => Class['mopensuse::zypper::repositories::devel-languages-python']
+    }
+    
+}
