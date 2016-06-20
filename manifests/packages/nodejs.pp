152641cb4000733c3d9b07ab86c74a2f2c487743
Marcin Morawski <marcin@morawskim.pl>
2016-06-20 17:22:28 +0200

    add localtunnel package

--- manifests/packages/nodejs.pp
+++ manifests/packages/nodejs.pp
@@ -1,37 +1,42 @@
 class mopensuse::packages::nodejs {
-  
+
   package {'nodejs':
     ensure => present
   }
-  
+
   package { 'jshint':
     ensure   => 'present',
     provider => 'npm',
     require  => Package['nodejs']
   }
-  
+
   package { 'grunt-cli':
     ensure   => 'present',
     provider => 'npm',
     require  => Package['nodejs']
   }
-  
+
   package { 'less':
     ensure   => 'present',
     provider => 'npm',
     require  => Package['nodejs']
   }
-  
+
   package { 'jslint':
     ensure   => 'present',
     provider => 'npm',
     require  => Package['nodejs']
   }
-  
+
   package { 'phantomjs':
     ensure   => 'present',
     provider => 'npm',
     require  => Package['nodejs']
   }
-  
+
+  package { 'localtunnel':
+    ensure   => 'present',
+    provider => 'npm',
+    require  => Package['nodejs']
+  }
 }
