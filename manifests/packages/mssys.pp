6ece950ad7d655e65db3508966134bb073085f36
Marcin Morawski <marcin@morawskim.pl>
2016-07-19 17:33:17 +0200

    ms-sys require packman repository

--- manifests/packages/mssys.pp
+++ manifests/packages/mssys.pp
@@ -1,7 +1,10 @@
 class mopensuse::packages::mssys {
 
+  include mopensuse::zypper::repositories::packman
+
     package {'ms-sys':
         ensure  => present,
+        require => Class['mopensuse::zypper::repositories::packman']
     }
 
 }
