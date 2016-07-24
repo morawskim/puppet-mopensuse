6080eb1763b10bd1ffb109b8daabaa8eb040a84c
Marcin Morawski <marcin@morawskim.pl>
2016-07-24 21:53:05 +0200

    jenkins reverse proxy

--- manifests/packages/jenkins.pp
+++ manifests/packages/jenkins.pp
@@ -1,4 +1,7 @@
-class mopensuse::packages::jenkins {
+class mopensuse::packages::jenkins(
+  $port=8880,
+  $reverse_proxying=true
+) {
 
   include mopensuse::zypper::repositories::jenkins
 
@@ -19,7 +22,7 @@ class mopensuse::packages::jenkins {
   
   augeas { "/files/etc/sysconfig/jenkins/JENKINS_PORT":
     changes => [
-    'set /files/etc/sysconfig/jenkins/JENKINS_PORT \'"8880"\'',
+    "set /files/etc/sysconfig/jenkins/JENKINS_PORT '\"${port}\"'",
     ],
     require => [ Package["augeas"], Package['jenkins'] ],
     notify  => Service['jenkins']
@@ -48,7 +51,11 @@ class mopensuse::packages::jenkins {
   jenkins::plugin { 'workflow-step-api':
       require => Package['jenkins'],
   }
-  
+
+  jenkins::plugin { 'scm-sync-configuration':
+      require => Package['jenkins'],
+  }
+
   # Dependencies for git plugin
   jenkins::plugin { ['credentials', 'mailer', 'scm-api', 'promoted-builds',
       'token-macro', 'parameterized-trigger', 'git-client', 'matrix-project',
@@ -79,5 +86,23 @@ class mopensuse::packages::jenkins {
       ensure       => present,
       ip           => '127.0.0.1',
       host_aliases => ['www.jenkins.dev']
+  }
+  
+  if $reverse_proxying {
+    include mopensuse::packages::apache2
+    include mopensuse::services::apache2
+
+    # Require proxy and proxy_http apache's modules
+    # Url must end with char "/"
+    $jenkins_url="http://localhost:${port}/"
+    file {'/etc/apache2/vhosts.d/jenkins.dev.conf':
+      ensure  => present,
+      mode    => '0744',
+      owner   => 'root',
+      group   => 'root',
+      content => template("${module_name}/jenkins/apache-vhost.erb"),
+      require => [ Class['mopensuse::packages::apache2'], Class['::jenkins'] ],
+      notify  => Class['mopensuse::services::apache2']
+    }
   }
 }
