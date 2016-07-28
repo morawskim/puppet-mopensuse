51e7cfaee3c33721e4aaba95ccffa1353946dcc6
Marcin Morawski <marcin@morawskim.pl>
2016-07-28 17:45:23 +0200

    notify apache and serveraliases

--- manifests/define/apache2_vhost.pp
+++ manifests/define/apache2_vhost.pp
@@ -1,15 +1,17 @@
 define mopensuse::define::apache2_vhost(
     $server_name,
     $document_root,
-    $vhost_path = $name,
-    $server_alias = "www.$server_name",
-    $server_admin = 'marcin@morawskim.pl'
+    $vhost_path    = $name,
+    $serveraliases = [],
+    $server_admin  = 'marcin@morawskim.pl',
+    $custom_config = []
 ) {
     validate_string($server_name)
     validate_absolute_path($document_root)
     validate_absolute_path($vhost_path)
-    validate_string($server_alias)
+    validate_array($serveraliases)
     validate_string($server_admin)
+    validate_array($custom_config)
 
     file { $vhost_path:
         ensure  => present,
@@ -17,5 +19,6 @@ define mopensuse::define::apache2_vhost(
         group   => 'root',
         mode    => '644',
         content => template("${module_name}/apache2/vhost-fpm.erb"),
+        notify  => Class['mopensuse::services::apache2']
     }
 }
