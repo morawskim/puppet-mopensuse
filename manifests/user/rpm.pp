ef9e0429aa06a79e8f61ab70a5bc7aa397ed987c
Marcin Morawski <marcin@morawskim.pl>
2016-08-13 10:25:05 +0200

    rpm user

---
+++ manifests/user/rpm.pp
@@ -0,0 +1,16 @@
+class mopensuse::user::rpm (
+    $username,
+    $user_home_path,
+    $password,
+    $realname,
+    
+    $ssh_keys = {},
+) {
+    mopensuse::user::account {$username:
+        username      => $username,
+        user_home_dir => $user_home_path,
+        password      => pw_hash($password, 'SHA-512', 'salt'), 
+        realname      => $realname,
+        sshkeys       => $ssh_keys
+    }
+}
