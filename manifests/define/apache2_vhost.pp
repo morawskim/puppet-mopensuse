3d39bc7742c7dd0a34e89d4bacb255cc6c761994
Marcin Morawski <marcin@morawskim.pl>
2016-09-13 12:24:41 +0200

    phpfpm config for apache2

--- manifests/define/apache2_vhost.pp
+++ manifests/define/apache2_vhost.pp
@@ -1,17 +1,23 @@
 define mopensuse::define::apache2_vhost(
     $server_name,
     $document_root,
-    $vhost_path    = $name,
-    $serveraliases = [],
-    $server_admin  = 'marcin@morawskim.pl',
-    $custom_config = []
+    $vhost_path     = $name,
+    $serveraliases  = [],
+    $server_admin   = 'marcin@morawskim.pl',
+    $custom_config  = [],
+    $php_fpm_socket = undef
 ) {
+    include mopensuse::services::apache2
+
     validate_string($server_name)
     validate_absolute_path($document_root)
     validate_absolute_path($vhost_path)
     validate_array($serveraliases)
     validate_string($server_admin)
     validate_array($custom_config)
+    if $php_fpm_socket {
+      validate_absolute_path($php_fpm_socket)
+    }
 
     file { $vhost_path:
         ensure  => present,
