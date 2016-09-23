9e622cd56576bc8947b57093d2ad1410f5327bc6
Marcin Morawski <marcin@morawskim.pl>
2016-09-23 22:04:37 +0200

    change line ending and php54-fpm service

--- manifests/packages/php54v.pp
+++ manifests/packages/php54v.pp
@@ -1,7 +1,16 @@
 class mopensuse::packages::php54v {
-  
+
   include mopensuse::zypper::repositories::morawskim
-  
+
+  $phpname = 'php54'
+  $service_name = "${phpname}-fpm"
+  $phpfpm_prefix = "/opt/php/${phpname}"
+  $pool_name = "${phpname}"
+  $pool_dir = "${phpfpm_prefix}/etc/php5/fpm/pool.d"
+  $pool_listen_owner = 'wwwrun'
+  $pool_listen_group = 'www'
+  $pool_listen_mode  = '0660'
+
   package {['php54', 'php54-fpm', 'php54-mysql', 'php54-ldap', 'php54-soap', 
     'php54-pgsql', 'php54-phar', 'php54-devel', 'php54-readline', 'php54-xdebug', 
     'php54-curl', 'php54-mbstring', 'php54-json', 'php54-intl', 'php54-gettext',
@@ -11,7 +20,7 @@ class mopensuse::packages::php54v {
     require => Class['mopensuse::zypper::repositories::morawskim'],
     notify  => Service['php-fpm']
   }
-  
+
   file {'/opt/php/php54/etc/php5/conf.d/custom.ini':
     ensure  => present,
     mode    => '0644',
@@ -20,5 +29,34 @@ class mopensuse::packages::php54v {
     source  => "puppet:///modules/${module_name}/php/php-custom.ini",
     notify  => [ Service['php-fpm'] ],
     require => Package['php54']
+  }
+
+  mopensuse::define::php_fpm {$phpname:
+    config_dir                     => "${phpfpm_prefix}/etc/php5/fpm",
+    config_name                    => 'php-fpm.conf',
+    config_template_file           => 'phpfpm/php-fpm.conf.erb',
+    service_name                   => "${phpname}-fpm",
+    pool_dir                       => $pool_dir,
+    pid_file                       => "/run/${phpname}-fpm.pid",
+    error_log                      => "/var/log/${phpname}-fpm.log",
+    syslog_ident                   => "${phpname}-fpm",
+  }
+
+  mopensuse::define::php_fpm_pool {$pool_name:
+    pool_dir          => $pool_dir,
+    pool_listen       => "/run/${pool_name}-fpm.sock",
+    pool_listen_owner => $pool_listen_owner,
+    pool_listen_group => $pool_listen_group,
+    pool_listen_mode  => $pool_listen_mode,
+    require           => Mopensuse::Define::Php_fpm[$phpname],
+    notify            => Service[$service_name]
+  }
+
+  service {$service_name:
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    require    => [ Mopensuse::Define::Php_fpm_pool[$pool_name] ]
   }
 }
