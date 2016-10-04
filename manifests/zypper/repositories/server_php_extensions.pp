742cff1655379262acf711babe83340463a9c7ce
Marcin Morawski <marcin@morawskim.pl>
2016-10-04 18:08:43 +0000

    change rpm key for server php extensions repo

--- manifests/zypper/repositories/server_php_extensions.pp
+++ manifests/zypper/repositories/server_php_extensions.pp
@@ -1,9 +1,9 @@
 class mopensuse::zypper::repositories::server_php_extensions {
-  
+
   include mopensuse::zypper::refresh
-  
+
   $gpg_key = "http://download.opensuse.org/repositories/server:/php:/extensions/openSUSE_${::operatingsystemrelease}/repodata/repomd.xml.key"
-  
+
   zypprepo {'server_php_extensions':
     baseurl      => "http://download.opensuse.org/repositories/server:/php:/extensions/openSUSE_${::operatingsystemrelease}/",
     enabled      => 1,
@@ -14,8 +14,8 @@ class mopensuse::zypper::repositories::server_php_extensions {
     type         => 'rpm-md',
     notify       => Class['mopensuse::zypper::refresh']
   }
-  
-  rpmkey {'DDCD7F1A':
+
+  rpmkey {'6B9D6523':
     ensure  => present,
     source  => $gpg_key,
     before  => Zypprepo['server_php_extensions']
