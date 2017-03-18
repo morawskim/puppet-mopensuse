2fa2b2258f79166011843791341a1d34a227c5bb
Marcin Morawski <marcin@morawskim.pl>
2017-03-18 10:07:36 +0100

    install mdless in user gem directory

---
+++ manifests/user/gem/mdless.pp
@@ -0,0 +1,8 @@
+class mopensuse::user::gem::mdless() {
+
+  package{'mdless':
+    ensure          => 'installed',
+    provider        => 'gem',
+    install_options => ['--user', '--user-install'],
+  }
+}
