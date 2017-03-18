465acb78d93135f9bcf2085684b7cabb76d64e90
Marcin Morawski <marcin@morawskim.pl>
2017-03-18 10:07:10 +0100

    install librarian-puppet in user gem directory

---
+++ manifests/user/gem/librarian_puppet.pp
@@ -0,0 +1,8 @@
+class mopensuse::user::gem::librarian_puppet() {
+
+  package{'librarian-puppet':
+    ensure          => 'installed',
+    provider        => 'gem',
+    install_options => ['--user', '--user-install'],
+  }
+}
