2dcc79f2c03785f7f4c9f2eee084453e5ebc54d4
Marcin Morawski <marcin@morawskim.pl>
2017-03-18 10:08:19 +0100

    install showterm in user gem directory

---
+++ manifests/user/gem/showterm.pp
@@ -0,0 +1,8 @@
+class mopensuse::user::gem::showterm() {
+
+  package{'showterm':
+    ensure          => 'installed',
+    provider        => 'gem',
+    install_options => ['--user', '--user-install'],
+  }
+}
