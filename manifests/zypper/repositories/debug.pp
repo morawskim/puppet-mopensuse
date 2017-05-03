307f5283fe20e9ab71566228a195a45ca1bcde46
Marcin Morawski <marcin@morawskim.pl>
2017-05-03 10:41:35 +0200

    opensuse debug repositories have different structure

--- manifests/zypper/repositories/debug.pp
+++ manifests/zypper/repositories/debug.pp
@@ -4,13 +4,15 @@ class mopensuse::zypper::repositories::debug(
   
   if $::operatingsystemrelease > 13.2 {
     $dist_key = "openSUSE_Leap_${::operatingsystemrelease}"
+    $path = "leap/${::operatingsystemrelease}"
   } else {
     $dist_key = "openSUSE_${::operatingsystemrelease}"
+    $path = "${::operatingsystemrelease}"
   }
   include mopensuse::zypper::refresh
 
   zypprepo {'debug':
-    baseurl      => "http://download.opensuse.org/debug/distribution//${::operatingsystemrelease}/repo/oss/",
+    baseurl      => "http://download.opensuse.org/debug/distribution/${path}/repo/oss/",
     enabled      => $enabled,
     autorefresh  => 1,
     descr        => "Debug packages for openSUSE (${dist_key})",
--- manifests/zypper/repositories/debug_update.pp
+++ manifests/zypper/repositories/debug_update.pp
@@ -4,13 +4,15 @@ class mopensuse::zypper::repositories::debug_update(
   
   if $::operatingsystemrelease > 13.2 {
     $dist_key = "openSUSE_Leap_${::operatingsystemrelease}"
+    $path = "leap/${::operatingsystemrelease}/oss"
   } else {
     $dist_key = "openSUSE_${::operatingsystemrelease}"
+    $path = "${::operatingsystemrelease}"
   }
   include mopensuse::zypper::refresh
 
   zypprepo {'debug_update':
-    baseurl      => "http://download.opensuse.org/debug/update/${::operatingsystemrelease}",
+    baseurl      => "http://download.opensuse.org/debug/update/${path}",
     enabled      => $enabled,
     autorefresh  => 1,
     descr        => "Debug packages updates for openSUSE (${dist_key})",
