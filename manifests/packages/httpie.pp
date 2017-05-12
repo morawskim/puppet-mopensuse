d7570ff5d16c9eb69ccccac136c52a9532e23773
Marcin Morawski <marcin@morawskim.pl>
2017-05-12 20:14:18 +0200

    install http-prompt

--- manifests/packages/httpie.pp
+++ manifests/packages/httpie.pp
@@ -1,11 +1,19 @@
 class mopensuse::packages::httpie {
 
   include mopensuse::zypper::repositories::devel-languages-python
+  include mopensuse::zypper::repositories::morawskim
 
   package {'httpie':
     ensure  => present,
     require => [
         Class['mopensuse::zypper::repositories::devel-languages-python']
+    ]
+  }
+
+  package {'http-prompt':
+    ensure  => present,
+    require => [
+        Class['mopensuse::zypper::repositories::morawskim']
     ]
   }
 }
