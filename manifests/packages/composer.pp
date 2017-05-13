b75611fc37a763c7a1fa590ae1dd90fb6000b2d5
Marcin Morawski <marcin@morawskim.pl>
2017-05-13 11:08:36 +0200

    composer refactoring

--- manifests/packages/composer.pp
+++ manifests/packages/composer.pp
@@ -1,33 +1,9 @@
-class mopensuse::packages::composer(
-  $github_token = undef
-) {
-  
-  include mopensuse::packages::php
-  include mopensuse::packages::vcs
-  include mopensuse::packages::wget
-  
-  class { '::composer':
-    target_dir      => '/usr/bin',
-    composer_file   => 'composer', # could also be 'composer.phar'
-    download_method => 'wget',     # or 'curl'
-    logoutput       => false,
-    tmp_path        => '/tmp',
-    php_package     => 'php5',
-    curl_package    => 'curl',
-    wget_package    => 'wget',
-    composer_home   => '/usr/local/share/composer',
-    php_bin         => 'php', # could also i.e. be 'php -d "apc.enable_cli=0"' for more fine grained control
-    suhosin_enabled => false,
-    auto_update     => false, # Set to true to automatically update composer to the latest version
-    github_token    => $github_token,
-    user            => 'root',
-    require         => [ 
-      Class['mopensuse::packages::php'],
-      Class['mopensuse::packages::vcs'],
-      Class['mopensuse::packages::wget']
-    ]
+class mopensuse::packages::composer() {
+
+  include mopensuse::zypper::repositories::morawskim
+
+  package {'composer':
+    ensure  => present,
+    require => Class['mopensuse::zypper::repositories::morawskim']
   }
-  
-  include mopensuse::packages::composer-symfony-packages
-  include mopensuse::packages::composer-packages
 }
