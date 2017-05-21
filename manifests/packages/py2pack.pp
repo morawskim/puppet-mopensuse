b3a6d761caa491c0f54b683f71bca0cdc57a8e83
Marcin Morawski <marcin@morawskim.pl>
2017-05-21 11:19:27 +0200

    we want newer version from python devel repository

--- manifests/packages/py2pack.pp
+++ manifests/packages/py2pack.pp
@@ -1,6 +1,9 @@
 class mopensuse::packages::py2pack {
 
+  include mopensuse::zypper::repositories::devel-languages-python
+
   package {'python-py2pack':
     ensure  => present,
+    require => Class['mopensuse::zypper::repositories::devel-languages-python']
   }
 }
