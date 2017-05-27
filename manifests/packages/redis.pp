aa01dab642525fb20af5a6666d7b68cddb937dbf
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 13:03:50 +0200

    change service name

--- manifests/packages/redis.pp
+++ manifests/packages/redis.pp
@@ -1,19 +1,25 @@
 class mopensuse::packages::redis {
-  
+
+  if $::operatingsystemrelease > 13.2 {
+    $servicename = 'redis@bin'
+  } else {
+    $servicename = 'redis'
+  }
+
   package {'redis':
     ensure => present
   }
-  
+
   file {'/etc/redis/bin.conf':
     ensure  => present,
     mode    => '0640',
     owner   => 'redis',
     group   => 'redis',
     source  => "puppet:///modules/${module_name}/redis/bin.conf",
-    notify  => [ Service['redis'] ],
+    notify  => [ Service[$servicename] ],
     require => Package['redis']
   }
-  
+
   file {'/etc/bash_completion.d/redis.sh':
     ensure  => present,
     mode    => '0644',
@@ -21,13 +27,12 @@ class mopensuse::packages::redis {
     group   => 'root',
     source  => "puppet:///modules/${module_name}/redis/bash_completion.sh"
   }
-  
-  service {'redis':
+
+  service {$servicename:
     ensure     => running,
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
     require    => [ Package['redis'], File['/etc/redis/bin.conf'] ]
   }
-  
 }
