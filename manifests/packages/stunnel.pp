0cf4b28d925837b71b186ae5286af9c83e40e6f6
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 13:05:39 +0200

    install from main repo

--- manifests/packages/stunnel.pp
+++ manifests/packages/stunnel.pp
@@ -1,10 +1,6 @@
 class mopensuse::packages::stunnel {
 
-  include mopensuse::zypper::repositories::packman
-
   package {'stunnel':
     ensure => present,
-    require => Class['mopensuse::zypper::repositories::packman'],
-    install_options => [ {'--repo' => 'packman'} ]
   }
 }
