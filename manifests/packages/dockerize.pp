0782317570108e0461020336b0c8414320c41e08
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 13:03:49 +0200

    add devel-languages-python repository as dependency

--- manifests/packages/dockerize.pp
+++ manifests/packages/dockerize.pp
@@ -1,9 +1,13 @@
 class mopensuse::packages::dockerize {
 
     include mopensuse::zypper::repositories::morawskim
+    include mopensuse::zypper::repositories::devel-languages-python
 
     package {'dockerize':
         ensure  => present,
-        require => Class['mopensuse::zypper::repositories::morawskim']
+        require => [
+          Class['mopensuse::zypper::repositories::morawskim'],
+          Class['mopensuse::zypper::repositories::devel-languages-python']
+        ]
     }
 }
