ec4ede53ed2763afff7e1dea9da14627a5f5fad9
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 13:03:48 +0200

    change package name

--- manifests/packages/gui/kdeconnect.pp
+++ manifests/packages/gui/kdeconnect.pp
@@ -1,18 +1,23 @@
 class mopensuse::packages::gui::kdeconnect {
-  
+
   include mopensuse::zypper::repositories::kde_extra
-  
-  package {'kdeconnect-kde4':
+
+  if $::operatingsystemrelease > 13.2 {
+    $packagename = 'kdeconnect-kde'
+  } else {
+    $packagename = 'kdeconnect-kde4'
+  }
+
+  package {$packagename:
     ensure  => present,
     require => Class['mopensuse::zypper::repositories::kde_extra']
   }
-  
+
   exec {'firewall_open_kdeconnect':
     command => 'sysconf_addword /etc/sysconfig/SuSEfirewall2 FW_CONFIGURATIONS_EXT kdeconnect-kde',
     unless  => 'grep "kdeconnect-kde" /etc/sysconfig/SuSEfirewall2 | grep "FW_CONFIGURATIONS_EXT"',
     path    => ['/usr/sbin', '/usr/bin'],
-    require => [ Package['kdeconnect-kde4'], Class['mopensuse::packages::firewall'] ],
+    require => [ Package[$packagename], Class['mopensuse::packages::firewall'] ],
     notify  => Class['mopensuse::services::firewall']
   }
 }
-
