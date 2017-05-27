f172dffc006abf831bcab51245c27c2a21c08a7d
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 13:03:49 +0200

    change package name

--- manifests/packages/gui/plasma-nm-openvpn.pp
+++ manifests/packages/gui/plasma-nm-openvpn.pp
@@ -1,8 +1,12 @@
 class mopensuse::packages::gui::plasma-nm-openvpn {
-  
-  package {'plasma-nm-openvpn':
+
+  if $::operatingsystemrelease > 13.2 {
+    $packagename = 'plasma-nm5-openvpn'
+  } else {
+    $packagename = 'plasma-nm-openvpn'
+  }
+
+  package {$packagename:
     ensure  => present,
   }
-  
 }
-
