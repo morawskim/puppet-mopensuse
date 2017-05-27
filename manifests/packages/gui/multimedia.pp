9af1443d61674f0abf21f28209d9b886d86d0df0
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 13:49:29 +0200

    refactoring

--- manifests/packages/gui/multimedia.pp
+++ manifests/packages/gui/multimedia.pp
@@ -1,23 +1,48 @@
 class mopensuse::packages::gui::multimedia {
-  
+
   include mopensuse::zypper::repositories::packman
   include mopensuse::zypper::repositories::libdvdcss
-  
-  package {['k3b-codecs', 'ffmpeg', 'lame', 'phonon-backend-vlc', 'phonon4qt5-backend-vlc',
-    'vlc-codecs', 'flash-player', 'libdvdcss2']:
+
+  package {['k3b-codecs', 'ffmpeg', 'lame', 'phonon-backend-vlc']:
     ensure  => present,
+    install_options => [ {'--from' => 'packman'} ],
     require => [ 
       Class['mopensuse::zypper::repositories::libdvdcss'],
       Class['mopensuse::zypper::repositories::packman']
     ]
   }
-  
-  package {['phonon-backend-gstreamer', 'phonon4qt5-backend-gstreamer']:
-    ensure => absent
-  }
-  
-}
 
+  package {['phonon4qt5-backend-vlc', 'vlc-codecs', 'flash-player', 'libdvdcss2']:
+    ensure  => present,
+    install_options => [ {'--from' => 'packman'} ],
+    require => [ 
+      Class['mopensuse::zypper::repositories::libdvdcss'],
+      Class['mopensuse::zypper::repositories::packman']
+    ]
+  }
+
+  package{'k3b':
+    ensure          => present,
+    before          => Package['k3b-codecs'],
+    install_options => [ {'--from' => 'packman'}, '--force' ]
+  }
+
+  package{'vlc-noX':
+    ensure          => present,
+    before          => Package['vlc-codecs'],
+    install_options => [ {'--from' => 'packman'}, '--force' ]
+  }
+
+  package {'libmusicbrainz4':
+    ensure => present,
+    before => Package['k3b-codecs']
+  }
+
+  package {'libdvbpsi9':
+    ensure => present,
+    before => Package['vlc-codecs']
+  }
+}
 
 
 
