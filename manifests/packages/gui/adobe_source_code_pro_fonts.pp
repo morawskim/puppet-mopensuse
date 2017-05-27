ea2f3c6f3fec83b0a7ae5a062fddeab3fed762ec
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 14:49:19 +0200

    on opensuse leap already exist package adobe-sourcecodepro-fonts

--- manifests/packages/gui/adobe_source_code_pro_fonts.pp
+++ manifests/packages/gui/adobe_source_code_pro_fonts.pp
@@ -1,9 +1,13 @@
 class mopensuse::packages::gui::adobe_source_code_pro_fonts {
 
-    include mopensuse::zypper::repositories::morawskim
-
-    package { 'adobe-source-code-pro-fonts':
-        ensure  => present,
-        require => Class['mopensuse::zypper::repositories::morawskim']
-    }
+  include mopensuse::zypper::repositories::morawskim
+  if $::operatingsystemrelease > 13.2 {
+    $packagename = 'adobe-sourcecodepro-fonts'
+  } else {
+    $packagename = 'adobe-source-code-pro-fonts'
+  }
+  package { $packagename:
+    ensure  => present,
+    require => Class['mopensuse::zypper::repositories::morawskim']
+  }
 }
