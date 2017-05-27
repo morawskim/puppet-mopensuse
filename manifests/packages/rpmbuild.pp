3a33503d41f9cdcf7144d04b8838bf9a5c79c33c
Marcin Morawski <marcin@morawskim.pl>
2017-05-27 13:03:50 +0200

    rpmlint

--- manifests/packages/rpmbuild.pp
+++ manifests/packages/rpmbuild.pp
@@ -2,7 +2,7 @@ class mopensuse::packages::rpmbuild {
 
   include mopensuse::zypper::repositories::devel_tools
 
-  package{['libtool', 'man', 'netcfg', 'patterns-openSUSE-base',
+  package{['libtool', 'man', 'netcfg', 'patterns-openSUSE-base', 'rpmlint',
     'patterns-openSUSE-devel_rpm_build', 'rpm-build', 'rpmdevtools', 'spectool']:
     ensure  => present,
     require => Class['mopensuse::zypper::repositories::devel_tools']
