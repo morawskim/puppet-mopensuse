204f32685623e68c4d8f1dd7523705ca40cd3365
Marcin Morawski <marcin@morawskim.pl>
2017-05-28 12:12:23 +0200

    install httrack from repository, not from local package

--- manifests/packages/httrack.pp
+++ manifests/packages/httrack.pp
@@ -1,21 +1,6 @@
 class mopensuse::packages::httrack {
 
-  $file = "ftp://rpmfind.net/linux/opensuse/distribution/${::operatingsystemrelease}/repo/oss/suse/${::architecture}/httrack-3.48.19-2.1.4.x86_64.rpm"
-  
-   wget::fetch {'download httrack':
-      source      => $file,
-      destination => '/tmp/httrack-suse.rpm',
-      timeout     => 0,
-      verbose     => false,
-      unless      => 'test -x /usr/bin/httrack'
-   }
-  
-   exec {'install httrack':
-      command => 'zypper -n in --auto-agree-with-licenses /tmp/httrack-suse.rpm',
-      path    => ['/usr/bin', '/bin'],
-      unless  => 'test -x /usr/bin/httrack',
-      require => Wget::Fetch['download httrack']
-   }
+  package {'httrack':
+      ensure => present
+  }
 }
-
-
