09066192bfb92668cc14abe7792e359a0ed48181
Marcin Morawski <marcin@morawskim.pl>
2017-05-28 12:01:14 +0200

    remove boost packages

--- manifests/packages/devel-c-cpp.pp
+++ manifests/packages/devel-c-cpp.pp
@@ -1,12 +1,11 @@
 class mopensuse::packages::devel-c-cpp {
-  
+
   include mopensuse::zypper::repositories::debug
   include mopensuse::zypper::repositories::debug_update
 
-  package {['boost-devel', 'boost-jam', 'glibc-info', 'ltrace', 'posix_cc',
+  package {['glibc-info', 'ltrace', 'posix_cc',
       'swig', 'valgrind', 'patterns-openSUSE-devel_C_C++',
       'patterns-openSUSE-devel_basis', 're2c']:
     ensure => present
   }
-  
 }
