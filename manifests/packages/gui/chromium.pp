6ca09851193f6689d6abde4033af328740da6fd2
Marcin Morawski <marcin@morawskim.pl>
2017-05-29 18:25:38 +0200

    remove chromium-ffmpeg

--- manifests/packages/gui/chromium.pp
+++ manifests/packages/gui/chromium.pp
@@ -2,13 +2,7 @@ class mopensuse::packages::gui::chromium {
 
   include mopensuse::zypper::repositories::packman
 
-  if $::operatingsystemrelease > 13.2 {
-    $packagename = 'chromium-ffmpegsumo'
-  } else {
-    $packagename = 'chromium-ffmpeg'
-  }
-
-  package{['chromium', $packagename]:
+  package{'chromium':
     ensure  => present,
     require => Class['mopensuse::zypper::repositories::packman']
   }
