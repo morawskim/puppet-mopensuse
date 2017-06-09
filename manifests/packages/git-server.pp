edf79e866388cd9b132a22bfb6ffbd1972ae4331
Marcin Morawski <marcin@morawskim.pl>
2017-06-09 18:17:39 +0200

    create file resources based on hieradata

--- manifests/packages/git-server.pp
+++ manifests/packages/git-server.pp
@@ -1,19 +1,25 @@
 class mopensuse::packages::git-server($ensure = 'present') {
-  
+
   include mopensuse::packages::vcs
   include mopensuse::packages::firewall
-  
+
   package {'git-daemon':
       ensure => $ensure,
   }
-  
+
   file {'/srv/git':
       ensure  => directory,
       mode    => '0755',
       owner   => 'root',
       group   => 'root',
   }
-  
+
+  create_resources(
+    'file',
+    hiera_hash('git_daemon_paths'),
+    {'ensure' => 'directory', 'require' => File['/srv/git']}
+  )
+
   service {'git-daemon':
       ensure     => running,
       enable     => true,
@@ -21,7 +27,7 @@ class mopensuse::packages::git-server($ensure = 'present') {
       hasstatus  => true,
       require    => Package['git-daemon']
   }
-  
+
   exec {'firewall_open_git_daemn_server_port':
       command => 'sysconf_addword /etc/sysconfig/SuSEfirewall2 FW_CONFIGURATIONS_EXT git-daemon',
       unless  => 'grep "git-daemon" /etc/sysconfig/SuSEfirewall2 | grep "FW_CONFIGURATIONS_EXT"',
