cb77cee96cb55399b23c9b950fa5f2d9b782cf17
Marcin Morawski <marcin@morawskim.pl>
2017-06-25 14:09:00 +0200

    Facter - phps
    Return map between PHP version and path where php binary is stored.
    Eg
    {
        "5" => "/opt/php/php56v/usr/bin",
        "5.6" => '/opt/php/php56v/usr/bin',
        ...
    }

---
+++ lib/facter/phps.rb
@@ -0,0 +1,46 @@
+Facter.add(:phps) do
+  confine :kernel => "Linux"
+  setcode do
+    phps = {}
+    php_versions = Facter.value(:php_versions)
+    php_versions.split(' ').each do |dir|
+        dirs = Dir.glob("#{dir}/*").select {|f| File.directory? f}
+        dirs.each do |dir|
+            php_bin = "#{dir}/usr/bin/php-config"
+            if File.file? php_bin
+                output = `#{php_bin} --version`
+                if output && !output.empty?
+                    version = output.chomp
+
+                    phps[version] = {:path => File.dirname(php_bin), :version => version}
+                    major, minor, patch = version.split('.')
+
+                    if phps[major] && Gem::Version.new(phps[major][:version]) < Gem::Version.new(version)
+                        phps[major] = phps[version]
+                    end
+
+                    if phps[major].nil?
+                        phps[major] = phps[version]
+                    end
+
+                    key = major + '.' + minor
+                    if phps[key] && Gem::Version.new(phps[key][:version]) < Gem::Version.new(version)
+                        phps[key] = phps[version]
+                    end
+
+                    if phps[key].nil?
+                        phps[key] = phps[version]
+                    end
+                end
+            end
+        end
+    end
+
+    data = {}
+    phps.each do |key, value|
+        data[key] = value[:path]
+    end
+    data
+  end
+end
+
