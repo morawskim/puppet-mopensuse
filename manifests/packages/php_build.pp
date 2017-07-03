21dfb5f4bd3ea6dabee2fb64045aea3a1127f569
Marcin Morawski <marcin@morawskim.pl>
2017-07-03 10:46:27 +0200

    php-build

---
+++ manifests/packages/php_build.pp
@@ -0,0 +1,9 @@
+class mopensuse::packages::php_build($ensure = 'present') {
+
+  include mopensuse::zypper::repositories::morawskim
+
+  package {'php-build':
+    ensure  => $ensure,
+    require => Class['mopensuse::zypper::repositories::morawskim']
+  }
+}
