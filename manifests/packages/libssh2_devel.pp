ac2458095a67302e16591e5e557f195b642a3991
Marcin Morawski <marcin@morawskim.pl>
2017-08-08 09:44:09 +0200

    fix build php5x-ssh2 on openSUSE 42.3

--- manifests/packages/libssh2_devel.pp
+++ manifests/packages/libssh2_devel.pp
@@ -3,18 +3,19 @@ class mopensuse::packages::libssh2_devel($ensure = 'present') {
   include mopensuse::zypper::repositories::devel_languages_c_cpp
 
   case $::operatingsystemrelease {
+    '42.3':           { $version = '1.8.0' }
     '42.2':           { $version = '1.7.0' }
     '13.2':           { $version = '1.6.0' }
     default:          { $version = 'latest' }
   }
-
+  #todo ensure only present or abset, if present set version
   package {['libssh2-devel']:
-    ensure  => $ensure,
+    ensure  => $version,
     install_options => [ {'--from' => 'devel_libraries_c_cpp'}, '--force' ],
   }
 
   package {['libssh2-1']:
-    ensure  => $ensure,
+    ensure  => $version,
     install_options => [ {'--from' => 'devel_libraries_c_cpp'}, '--force' ],
     require => Class['mopensuse::zypper::repositories::devel_languages_c_cpp'],
     before  => Package['libssh2-devel'],
