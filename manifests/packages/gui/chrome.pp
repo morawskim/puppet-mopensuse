4c7dc232c81af66114727db4cec8386c7af83cc5
Marcin Morawski <marcin.morawski@sensilabs.pl>
2017-08-10 14:37:27 +0200

    Repo of chrome is signed by subkey. Download chrome from other repo.

--- manifests/packages/gui/chrome.pp
+++ manifests/packages/gui/chrome.pp
@@ -1,9 +1,11 @@
 class mopensuse::packages::gui::chrome($ensure = 'present') {
   
   include mopensuse::zypper::repositories::google-chrome
+  include mopensuse::zypper::repositories::morawskim
   
   package{'google-chrome-stable':
     ensure  => present,
-    require => Class['mopensuse::zypper::repositories::google-chrome'],
+    require => Class['mopensuse::zypper::repositories::morawskim'],
+    notify => Class['mopensuse::zypper::repositories::google-chrome']
   }
 }
