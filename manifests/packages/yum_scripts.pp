ccbdd6c64637572b662ff08e28559e59d4db0916
Marcin Morawski <marcin.morawski@sensilabs.pl>
2017-08-10 14:37:37 +0200

    yum-scripts - require morawskim repository

--- manifests/packages/yum_scripts.pp
+++ manifests/packages/yum_scripts.pp
@@ -1,6 +1,9 @@
 class mopensuse::packages::yum_scripts($ensure = 'present') {
 
+  include mopensuse::zypper::repositories::morawskim
+
   package {'yum-scripts':
-    ensure          => $ensure,
+    ensure  => $ensure,
+    require => Class['mopensuse::zypper::repositories::morawskim']
   }
 }
