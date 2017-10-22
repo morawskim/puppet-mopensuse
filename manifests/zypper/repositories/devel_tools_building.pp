e13dbad60832195a7468879f702c61bf88d279a1
Marcin Morawski <marcin@morawskim.pl>
2017-10-22 09:44:10 +0200

    add mopensuse::zypper::repositories::devel_tools_building

---
+++ manifests/zypper/repositories/devel_tools_building.pp
@@ -0,0 +1,16 @@
+class mopensuse::zypper::repositories::devel_tools_building {
+
+  include mopensuse::zypper::refresh
+  include mopensuse::rpmkeys::devel_tools
+
+  Zypprepo {
+    notify => Class['mopensuse::zypper::refresh'],
+    require => Class['mopensuse::rpmkeys::devel_tools'],
+  }
+
+  ensure_resource(
+    'zypprepo',
+    'devel_tools_building',
+    hiera_hash('mopensuse::zypper::repositories::devel_tools_building')
+  )
+}
