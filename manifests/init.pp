ecebb36343a5f67c8ee3ba398d10b7c409a9a4bf
Marcin Morawski <marcin@morawskim.pl>
2017-10-23 18:24:08 +0200

    remove httpie class
    Use pipsi to install python applications in isolated environment

--- manifests/init.pp
+++ manifests/init.pp
@@ -74,7 +74,6 @@ class mopensuse {
   include mopensuse::packages::hstr
   include mopensuse::packages::html2text
   include mopensuse::packages::httperf
-  include mopensuse::packages::httpie
   include mopensuse::packages::httpry
   include mopensuse::packages::httrack
   include mopensuse::packages::hub
