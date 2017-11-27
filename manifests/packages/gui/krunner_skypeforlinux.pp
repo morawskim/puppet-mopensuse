704a9d0af44bb0809a51688958118a9924b9f927
Marcin Morawski <marcin@morawskim.pl>
2017-11-27 09:51:17 +0100

    install krunner-skypeforlinux

---
+++ manifests/packages/gui/krunner_skypeforlinux.pp
@@ -0,0 +1,9 @@
+class mopensuse::packages::gui::krunner_skypeforlinux($ensure = 'present') {
+
+  include mopensuse::zypper::repositories::morawskim
+
+  package {'krunner-skypeforlinux':
+    ensure  => $ensure,
+    require => Class['mopensuse::zypper::repositories::morawskim']
+  }
+}
--- manifests/packages/gui/skypeforlinux.pp
+++ manifests/packages/gui/skypeforlinux.pp
@@ -1,6 +1,7 @@
 class mopensuse::packages::gui::skypeforlinux($ensure = 'present') {
 
   include mopensuse::zypper::repositories::skype_stable
+  include mopensuse::packages::gui::krunner_skypeforlinux
 
   package {'skypeforlinux':
       ensure  => 'present',
