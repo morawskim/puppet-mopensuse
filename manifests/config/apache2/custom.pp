51fd496fc7496603929e8d3266a37ccd6e27ad0c
Marcin Morawski <marcin@morawskim.pl>
2018-05-07 18:56:39 +0200

    Apache2 - custom config

---
+++ manifests/config/apache2/custom.pp
@@ -0,0 +1,14 @@
+class mopensuse::config::apache2::custom($files) {
+  include mopensuse::packages::apache2
+  include mopensuse::services::apache2
+
+  create_resources(
+    'file',
+    $files,
+    {
+      'ensure'  => 'present',
+      'notify'  => Class['mopensuse::services::apache2'],
+      'require' => Class['mopensuse::packages::apache2'],
+    }
+  )
+}
