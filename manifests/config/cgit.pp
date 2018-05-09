ec83b28857ff9aeeb1762c016e75792f8b752020
Marcin Morawski <marcin@morawskim.pl>
2018-05-09 20:11:57 +0200

    cgit - config

---
+++ manifests/config/cgit.pp
@@ -0,0 +1,24 @@
+class mopensuse::config::cgit(
+    $files,
+    $cgit_host,
+    $cgit_host_aliases = []
+) {
+  include mopensuse::packages::cgit
+  include mopensuse::services::apache2
+
+  create_resources(
+    'file',
+    $files,
+    {
+      'ensure'  => 'present',
+      'require' => Class['mopensuse::packages::cgit'],
+      'notify'  => Class['mopensuse::services::apache2']
+    }
+  )
+
+  host {$cgit_host:
+    ensure     => present,
+    ip       => '127.0.0.1',
+    host_aliases => $cgit_host_aliases
+  }
+}
