64e5f5ae8a077de40e93e46441b3332b84507155
Marcin Morawski <marcin@morawskim.pl>
2018-05-12 12:29:45 +0200

    dnsmasq config

---
+++ manifests/config/dnsmasq.pp
@@ -0,0 +1,12 @@
+class mopensuse::config::dnsmasq($files) {
+  include mopensuse::packages::dnsmasq
+
+  create_resources(
+    'file',
+    $files,
+    {
+      'ensure'  => 'present',
+      'require' => Class['mopensuse::packages::dnsmasq'],
+    }
+  )
+}
