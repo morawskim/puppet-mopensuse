01eedc6d316ca4a506f6984f4900c2db91f78263
Marcin Morawski <marcin@morawskim.pl>
2018-05-23 22:19:00 +0200

    Reformat code

--- lib/puppet/provider/a2mod/suse.rb
+++ lib/puppet/provider/a2mod/suse.rb
@@ -1,30 +1,30 @@
 require 'puppet/provider/a2mod'
 
-Puppet::Type.type(:a2mod).provide(:suse, :parent => Puppet::Provider::A2mod) do
-    desc "Manage Apache 2 modules on openSuSE"
+Puppet::Type.type(:a2mod).provide(:suse, parent: Puppet::Provider::A2mod) do
+  desc 'Manage Apache 2 modules on openSuSE'
 
-    commands :encmd => "a2enmod"
+  commands encmd: 'a2enmod'
 
-    confine :osfamily => :suse
-    defaultfor :operatingsystem => [:OpenSuSE]
+  confine osfamily: :suse
+  defaultfor operatingsystem: [:OpenSuSE]
 
-    def self.instances
-      modules = encmd("-l").split(' ')
+  def self.instances
+    modules = encmd('-l').split(' ')
 
-      modules.map do |mod|
-        new(
-          :name     => mod,
-          :ensure   => :present,
-          :provider => :suse
-        )
-      end
+    modules.map do |mod|
+      new(
+        name: mod,
+        ensure: :present,
+        provider: :suse
+      )
     end
+  end
 
-    def create
-        encmd resource[:name]
-    end
+  def create
+    encmd resource[:name]
+  end
 
-    def destroy
-        encmd '-d', resource[:name]
-    end
+  def destroy
+    encmd '-d', resource[:name]
+  end
 end
