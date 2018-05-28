77433a5d906be971098c3b3b58d644fad26c4454
Marcin Morawski <marcin@morawskim.pl>
2018-05-28 18:54:47 +0200

    Extract open ssh port to new class

--- manifests/packages/sshd.pp
+++ manifests/packages/sshd.pp
@@ -3,14 +3,6 @@ class mopensuse::packages::sshd (
 ) {
   include mopensuse::services::sshd
 
-  #firewall ssh
-  exec { 'firewall_open_sshd_port':
-    command => 'sysconf_addword /etc/sysconfig/SuSEfirewall2 FW_CONFIGURATIONS_EXT sshd',
-    unless  => 'grep "sshd" /etc/sysconfig/SuSEfirewall2 | grep "FW_CONFIGURATIONS_EXT"',
-    path    => ['/usr/sbin', '/usr/bin'],
-    require => [ Package['openssh'] ]
-  }
-
   package { 'openssh':
     ensure => $ensure,
     notify => Class['mopensuse::services::sshd']
