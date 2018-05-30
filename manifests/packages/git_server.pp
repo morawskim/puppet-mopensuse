4602203b0f2dde9968c5f5924e29cd2347a7455f
Marcin Morawski <marcin@morawskim.pl>
2018-05-30 19:06:55 +0200

    Extract open git-daemon port to new classes

--- manifests/packages/git_server.pp
+++ manifests/packages/git_server.pp
@@ -16,11 +16,4 @@ class mopensuse::packages::git_server (
     owner  => 'root',
     group  => 'root',
   }
-
-  exec { 'firewall_open_git_daemn_server_port':
-    command => 'sysconf_addword /etc/sysconfig/SuSEfirewall2 FW_CONFIGURATIONS_EXT git-daemon',
-    unless  => 'grep "git-daemon" /etc/sysconfig/SuSEfirewall2 | grep "FW_CONFIGURATIONS_EXT"',
-    path    => ['/usr/sbin', '/usr/bin'],
-    require => [ Package['git-daemon'], Class['mopensuse::packages::firewall'] ]
-  }
 }
