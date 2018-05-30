980904b01f70a54a7cc9b1c8d5c4081b2529abb9
Marcin Morawski <marcin@morawskim.pl>
2018-05-30 19:07:19 +0200

    Extract open mailhog-http port to new classes

--- manifests/packages/mailhog.pp
+++ manifests/packages/mailhog.pp
@@ -6,12 +6,4 @@ class mopensuse::packages::mailhog (
   package { 'mailhog':
     ensure => $ensure
   }
-
-  exec { 'firewall_open_http_mailhog_port':
-    command => 'sysconf_addword /etc/sysconfig/SuSEfirewall2 FW_CONFIGURATIONS_EXT mailhog-http',
-    unless  => 'grep "mailhog-http" /etc/sysconfig/SuSEfirewall2 | grep "FW_CONFIGURATIONS_EXT"',
-    path    => ['/usr/sbin', '/usr/bin'],
-    require => [ Package['mailhog'], Class['mopensuse::packages::firewall'] ],
-    notify  => Class['mopensuse::services::firewall']
-  }
 }
