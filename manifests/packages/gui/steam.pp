9fdc9568418a7fc22bcdc81b474e501c3c6b6df5
Marcin Morawski <marcin@morawskim.pl>
2018-06-01 10:18:12 +0200

    Extract open steam streaming port to new class

--- manifests/packages/gui/steam.pp
+++ manifests/packages/gui/steam.pp
@@ -9,12 +9,4 @@ class mopensuse::packages::gui::steam (
     ensure  => $ensure,
     require => Class['mopensuse::zypper::repositories::games_tools']
   }
-
-  exec { 'firewall_open_steam_streaming':
-    command => 'sysconf_addword /etc/sysconfig/SuSEfirewall2 FW_CONFIGURATIONS_EXT steam-streaming',
-    unless  => 'grep "steam-streaming" /etc/sysconfig/SuSEfirewall2 | grep "FW_CONFIGURATIONS_EXT"',
-    path    => ['/usr/sbin', '/usr/bin'],
-    require => [ /*Class['mopensuse::config::steam'],*/ Class['mopensuse::packages::firewall'] ],
-    notify  => Class['mopensuse::services::firewall']
-  }
 }
