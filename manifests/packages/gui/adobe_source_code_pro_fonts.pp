30bbbfa95a31bc1290768c8798cf1a00fa793038
Marcin Morawski <marcin@morawskim.pl>
2018-08-02 20:28:53 +0200

    Fix Error: Evaluation Error on puppet5
    Error: Evaluation Error: Comparison of: String > Float, is not possible. Caused by 'A String is not comparable to a non String'.

--- manifests/packages/gui/adobe_source_code_pro_fonts.pp
+++ manifests/packages/gui/adobe_source_code_pro_fonts.pp
@@ -2,14 +2,8 @@ class mopensuse::packages::gui::adobe_source_code_pro_fonts (
   $ensure = 'present'
 ) {
 
-  include mopensuse::zypper::repositories::morawskim
-  if $::operatingsystemrelease > 13.2 {
-    $packagename = 'adobe-sourcecodepro-fonts'
-  } else {
-    $packagename = 'adobe-source-code-pro-fonts'
-  }
+  $packagename = 'adobe-sourcecodepro-fonts'
   package { $packagename:
     ensure  => $ensure,
-    require => Class['mopensuse::zypper::repositories::morawskim']
   }
 }
