934666e5410bf8451e5f8fbdd3701c51bbfd1b01
Marcin Morawski <marcin@morawskim.pl>
2018-08-05 13:24:33 +0200

    my.cnf - Move settings to hieradata

--- manifests/user/config/my_cnf.pp
+++ manifests/user/config/my_cnf.pp
@@ -1,27 +1,10 @@
 define mopensuse::user::config::my_cnf (
+  $settings,
   $my_cnf_path
 ) {
 
-  ini_setting { "${my_cnf_path} - set root password":
-    ensure  => present,
-    path    => $my_cnf_path,
-    section => 'client',
-    setting => 'password',
-    value   => hiera('mopensuse::packages::mysql::mysql_root_password',
-      'no_set_value_for_mopensuse::packages::mysql::mysql_root_password_in_hieradata'),
-  }
-
-  ini_setting { "${my_cnf_path} - set root username":
-    ensure  => present,
-    path    => $my_cnf_path,
-    section => 'client',
-    setting => 'user',
-    value   => hiera('mopensuse::packages::mysql::mysql_root_username', 'root'),
-    before  => Ini_setting["${my_cnf_path} - set root password"]
-  }
-
-  file { $my_cnf_path:
-    ensure => present,
-    mode   => '0600',
-  }
+  validate_hash($settings)
+  # todo mmo - debug why I cant set path in hieradata
+  $defaults = {path => $my_cnf_path}
+  create_ini_settings($settings, $defaults)
 }
