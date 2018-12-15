a189414155fa604f0349de703685ae39f1bd60ac
Marcin Morawski <marcin@morawskim.pl>
2018-12-15 14:01:15 +0100

    For opensuse leap 15 we dont need add ruby extensions repo for rubygem-augeas

--- manifests/packages/augeas.pp
+++ manifests/packages/augeas.pp
@@ -1,11 +1,9 @@
 class mopensuse::packages::augeas (
   $ensure = 'present'
 ) {
-  include mopensuse::zypper::repositories::devel_languages_ruby_extensions
 
   package { "ruby${::rubydefverion['majversion']}-rubygem-ruby-augeas":
     ensure  => present,
-    require => Class['mopensuse::zypper::repositories::devel_languages_ruby_extensions'],
     before  => Package['augeas']
   }
 
